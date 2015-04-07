5c338e5d067bf335fcd90e0ee90d71c7b41e8cfe
KramerLewis <kramer.lewis94@gmail.com>
2015-04-07 11:55:06 -0700

    commit

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -3,7 +3,8 @@ package com.tedneward.example;
 import java.beans.*;
 import java.util.*;
 
-public class Person {
+public class Person implements Comparable<Person>{
+  private static int count = 0;
   private int age;
   private String name;
   private double salary;
@@ -15,21 +16,45 @@ public class Person {
   }
   
   public Person(String n, int a, double s) {
+    count++;
     name = n;
     age = a;
     salary = s;
+    ssn="";
+  }
+
+  public static int getCount() {
+    return count;
   }
 
   public int getAge() {
     return age;
   }
+
+  public void setAge(int value) {
+    if(value <= 0) {
+      throw new IllegalArgumentException("age cannot be less than 0");
+    }
+    age = value;
+  }
   
   public String getName() {
     return name;
   }
+
+  public void setName(String value) {
+    if(value == null) {
+      throw new IllegalArgumentException("name cannot be null");
+    }
+    name = value;
+  }
   
   public double getSalary() {
     return salary;
+  }
+
+  public void setSalary(double value) {
+    salary = value;
   }
   
   public String getSSN() {
@@ -58,12 +83,50 @@ public class Person {
     return age + 10;
   }
   
-  public boolean equals(Person other) {
-    return (this.name.equals(p.name) && this.age == p.age);
+  @Override
+  public boolean equals(Object o) {
+    if (o == null || !(o instanceof Person)) {
+      return false;
+    }
+    Person other = (Person)o;
+    return (name.equals(other.name) && age == other.age);
   }
 
-  public String tostring() {
-    return "{{FIXME}}";
+  @Override
+  public String toString() {
+    return "[Person name:" + name + " age:" + age + " salary:" + salary + "]";
+  }
+
+  public int compareTo(Person other) {
+    double result = other.salary - this.salary;
+    if(result < 0) {
+      return -1;
+    } else if(result > 0) {
+      return 1;
+    } else {
+      return 0;
+    }
+  }
+
+  public static ArrayList<Person> getNewardFamily() {
+    ArrayList<Person> fam = new ArrayList<Person>();
+    fam.add(new Person("Ted", 41, 250000));
+    fam.add(new Person("Charlotte", 43, 150000));
+    fam.add(new Person("Michael", 22, 10000));
+    fam.add(new Person("Matthew", 15, 0));
+    return fam;
+  }
+
+
+  public static class AgeComparator implements Comparator {
+
+    @Override
+    public int compare(Object a1, Object b1) {
+      Person a = (Person)a1;
+      Person b = (Person)b1;
+      return a.getAge() - b.getAge();
+    }
+
   }
 
   // PropertyChangeListener support; you shouldn't need to change any of
